af5e4304acaef01520cfb8033342848464275ca9
Sheshadri <kvs24161@gmail.com>
2025-11-28 17:05:18 +0530

    Responsive Gallery Project Using the React

--- galleryproject/src/App.jsx
+++ galleryproject/src/App.jsx
@@ -1,28 +1,69 @@
 import axios from "axios"
 import { useState } from "react";
+import { useEffect } from "react";
 
 
 
-function App(){
-  const[UserData,setUserData]=useState([]);
-  async function getData(){
-    let resp = await axios.get('https://picsum.photos/v2/list?page=2&limit=300');
+function App() {
+  const [UserData, setUserData] = useState([]);
+  const [index, setIndex] = useState(1);
+
+  async function getData() {
+    let resp = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=5`);
     console.log(resp.data);
     setUserData(resp.data);
   }
-  
 
-  
+
+
+  let printUserData = "No Data Found";
+  if (UserData.length > 0) {
+    printUserData = UserData.map((elem, idx) => {
+      return (
+        <div className="h-60" key={idx}>
+          <a href={elem.url}>
+            <img className="h-full w-full object-cover  rounded-2xl  " src={elem.download_url} alt="" />
+            <h2 className="text-white text-2xl ">{elem.author}</h2>
+          </a>
+
+        </div>
+      )
+    })
+  }
+
+  useEffect(() => {
+    getData();
+  }, [index])
+
+
+
   return (<>
-  <button onClick={getData} className="bg-green-400 text-white px-2 py-2 ml-3 mt-5">Get Data</button>
-  <div className="flex flex-wrap gap-5 p-5"> 
-   {UserData.map((ele,idx)=>{
-     return <div className="">
-      <img key={idx} className="h-60 w-60 rounded-xl object-cover cursor-pointer" src={ele.download_url} alt=""  />
-      <h2 className="font-medium text-2xl">{ele.author}</h2>
-     </div>
-   })}
-  </div>
+    <div className="h-full ">
+      < div className="bg-black  h-full p-5">
+        {/* <h2 className="text- fixed bg-amber-600  w-4 p-1">{index}</h2> */}
+        <div className="grid grid-col-1 lg:grid-cols-4 sm:grid-cols-2 gap-8">
+          {printUserData}
+        </div>
+        <div className="flex justify-center items-center mt-6 gap-6">
+          <button className="text-black bg-amber-200 active:scale-95 px-7 py-2 rounded cursor-pointer"
+            onClick={() => {
+              if (index > 1) {
+                setIndex(index - 1);
+              }
+              // setIndex(index-1)
+            }}>Prev</button>
+            <h2 className="text-white">Page {index}</h2>
+          <button className="text-black bg-amber-200 active:scale-95 px-7 py-2 rounded cursor-pointer"
+            onClick={() => {
+              setIndex(index + 1);
+            }
+            }>Next</button>
+        </div>
+
+      </div>
+    </div>
+
+
   </>
   )
 }
