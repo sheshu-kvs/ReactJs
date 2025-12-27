608d8482f27ea1147006c5ca87d34c3c27498691
Sheshadri <kvs24161@gmail.com>
2025-12-27 22:48:03 +0530

    Responsive With Moblie Using Tailwind CSS

---
+++ Responsive/responsive/src/App.jsx
@@ -0,0 +1,16 @@
+function App(){
+  return <>
+  <h2 className="text-xl md:text-2xl lg:text-4xl">Hello</h2>
+  <div className="flex flex-col md:flex-row h-1/2 w-full gap-4 px-2 text-center">
+    <div className="bg-orange-400 text-xl w-2xl h-24 border-3 border-black flex justify-center items-center">box</div>
+    <div className="bg-orange-400 text-xl w-2xl h-24 border-3 border-black">box</div>
+    <div className="bg-orange-400 text-xl w-2xl h-24 border-3 border-black">box</div>
+    <div className="bg-orange-400 text-xl w-2xl h-24 border-3 border-black">box</div>
+    <div className="bg-orange-400 text-xl w-2xl h-24 border-3 border-black">box</div>
+    <div className="bg-orange-400 text-xl w-2xl h-24 border-3 border-black">box</div>
+  </div>
+  </>
+}
+
+
+export default App
