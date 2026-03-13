0f17635edfc30cc0ec6556a97b89b56b4a0606e5
Sheshadri <kvs24161@gmail.com>
2026-03-13 22:58:56 +0530

    ES6

---
+++ React_Learning/Day1Es6/src/App.jsx
@@ -0,0 +1,16 @@
+function App(){
+  const numbers = [1,2,3,4];
+  
+  return <>
+  <div className="bg-amber-500 p-2">
+      <div className=" p-3 m-2 rounded-md w-[350px] font-bold bg-white text-black text-2xl">
+         <h2>Sheshadri Kumar</h2>
+         {numbers.map(x){
+             
+         }}
+      </div>
+  </div>
+  </>
+}
+
+export default App
