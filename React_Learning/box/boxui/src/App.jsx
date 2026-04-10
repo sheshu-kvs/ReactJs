c5fbb458bbae1bfb4ad11e709b771fa4c98f1a10
Sheshadri <kvs24161@gmail.com>
2026-04-10 22:53:53 +0530

    ui box

---
+++ React_Learning/box/boxui/src/App.jsx
@@ -0,0 +1,22 @@
+function App(){
+  return <>
+ <div className="bg-gray-400 h-screen">
+   <h2 className="text-center text-2xl text-center font-bold">This is the reactUI</h2>
+
+  <div className="grid grid-cols-1 md:grid-cols-4 gap-4 p-8 mt-3">
+    <div className="bg-white border-0 rounded-2xl h-72 w-72 flex justify-center items-center">box 1</div>
+    <div className="bg-white border-0 rounded-2xl h-72 w-3xs">box 2</div>
+    <div className="bg-white border-0 rounded-2xl h-72 w-3xs">box 3</div>
+    <div className="bg-white border-0 rounded-2xl h-72 w-3xs">box 4</div>
+    <div className="bg-white border-0 rounded-2xl h-72 w-3xs">box 5</div>
+    <div className="bg-white border-0 rounded-2xl h-72 w-3xs">box 6</div>
+    <div className="bg-white border-0 rounded-2xl h-72 w-3xs">box 7</div>
+    <div className="bg-white border-0 rounded-2xl h-72 w-3xs">box 8</div>
+  </div>
+ </div>
+  </>
+}
+
+
+
+export default App
